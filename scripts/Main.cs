ac7437695d727e6dc63f8c900afa44f759a2b374
Jonny <jmanly784@gmail.com>
2025-06-29 11:29:42 -0700

    Added UiManager. Ui opening now disables movement.  #7

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,29 +1,38 @@
 using Godot;
+using Planets.UI;
 using System;
 
 namespace Planets
 {
-	public partial class Main : Node
-	{
-		[Export]
-		public bool Generated { get; set; } = false;
-		// Called when the node enters the scene tree for the first time.
-		public override void _Ready()
-		{
-			if (!Generated)
-			{
-				var p = SystemGenerator.PlanetGenerator.GeneratePlanet();
-				GetNode<Node3D>("%World").AddChild(p);
-				p.GetChild<MeshInstance3D>(0).Position = new Vector3(0, 0, -1300f);
-				PackedScene scene = new();
-				scene.Pack(p);
-				ResourceSaver.Save(scene, "res://TestPlanet.tscn", ResourceSaver.SaverFlags.Compress);
-			}
-		}
+    public partial class Main : Node
+    {
+        [Export]
+        public bool Generated { get; set; } = false;
+        // [Export]
+        // public PackedScene UI { get; set; }
 
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta)
-		{
-		}
-	}
+        public MainUi Ui { get; private set; } = null;
+        // Called when the node enters the scene tree for the first time.
+
+        public override void _Ready()
+        {
+            Ui = (MainUi)GetNode<InstancePlaceholder>("UI").CreateInstance();
+            UiManager.Instance.Ui = Ui;
+            // AddChild(Ui);
+            if (!Generated)
+            {
+                var p = SystemGenerator.PlanetGenerator.GeneratePlanet();
+                GetNode<Node3D>("%World").AddChild(p);
+                p.GetChild<MeshInstance3D>(0).Position = new Vector3(0, 0, -1300f);
+                PackedScene scene = new();
+                scene.Pack(p);
+                ResourceSaver.Save(scene, "res://TestPlanet.tscn", ResourceSaver.SaverFlags.Compress);
+            }
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+        }
+    }
 }
--- scripts/ui/GameMenu.cs
+++ scripts/ui/GameMenu.cs
@@ -2,56 +2,62 @@ using Godot;
 using System;
 using System.Threading.Tasks;
 
-public partial class GameMenu : Control
+namespace Planets.UI
 {
-    [Export]
-    public Button ExitButton { get; set; } = null;
-    private ConfirmationDialog _exitDialog;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public partial class GameMenu : Control
     {
-        _exitDialog = null;
-        Hide();
-        ExitButton.Pressed += OnExitButtonPressed;
-    }
+        [Export]
+        public Button ExitButton { get; set; } = null;
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
-
-    private void OnExitButtonPressed()
-    {
-        GD.Print("Exit button pressed");
-        if (_exitDialog == null)
-            _ = ShowExitDialog();
-    }
-
-    private async Task ShowExitDialog()
-    {
-        CreateExitDialog();
-        AddSibling(_exitDialog);
-        _exitDialog.PopupCentered();
-        _exitDialog.GrabFocus();
-        _exitDialog.Confirmed += () =>
+        [Export]
+        public Button SettingsButton { get; set; } = null;
+        private ConfirmationDialog _exitDialog;
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
         {
-            GetTree().Root.PropagateNotification((int)NotificationWMCloseRequest);
-            GetTree().Quit();
-        };
-        _ = await ToSignal(_exitDialog, ConfirmationDialog.SignalName.VisibilityChanged);
-        _exitDialog = null;
-    }
+            _exitDialog = null;
+            Hide();
+            ExitButton.Pressed += OnExitButtonPressed;
+        }
 
-    private void CreateExitDialog()
-    {
-        ConfirmationDialog dialog = new();
-        dialog.AlwaysOnTop = true;
-        dialog.Title = "Exit?";
-        dialog.DialogText = "Are you sure you want to exit?";
-        dialog.OkButtonText = "Yes";
-        dialog.CancelButtonText = "No";
-        dialog.Transient = true;
-        dialog.Exclusive = true;
-        _exitDialog = dialog;
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+        }
+
+        private void OnExitButtonPressed()
+        {
+            GD.Print("Exit button pressed");
+            if (_exitDialog == null)
+                _ = ShowExitDialog();
+        }
+
+        private async Task ShowExitDialog()
+        {
+            CreateExitDialog();
+            AddSibling(_exitDialog);
+            _exitDialog.PopupCentered();
+            _exitDialog.GrabFocus();
+            _exitDialog.Confirmed += () =>
+            {
+                GetTree().Root.PropagateNotification((int)NotificationWMCloseRequest);
+                GetTree().Quit();
+            };
+            _ = await ToSignal(_exitDialog, ConfirmationDialog.SignalName.VisibilityChanged);
+            _exitDialog = null;
+        }
+
+        private void CreateExitDialog()
+        {
+            ConfirmationDialog dialog = new();
+            dialog.AlwaysOnTop = true;
+            dialog.Title = "Exit?";
+            dialog.DialogText = "Are you sure you want to exit?";
+            dialog.OkButtonText = "Yes";
+            dialog.CancelButtonText = "No";
+            dialog.Transient = true;
+            dialog.Exclusive = true;
+            _exitDialog = dialog;
+        }
     }
 }
