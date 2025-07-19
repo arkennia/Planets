ddd6fcf2d253970e8a34913453e5c9bdf9799da1
Jonny <jmanly784@gmail.com>
2025-07-18 20:11:37 -0700

    Adding Terrain3D and moving files around.

---
+++ scripts/SystemGenerator/Terrain/Terrain3D.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace Planets.SystemGenerator.Terrain;
+
+public partial class Terrain3D : MeshInstance3D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
